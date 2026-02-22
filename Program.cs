5387ad16c16a2b205c871a12b55cf51fc691d2e1
emanu <emanu@EMANUEL_RAMREZ>
2026-02-22 09:16:03 -0600

    Entradas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
 ﻿// 3) Sistema de facturación con control antifraude
+Console.WriteLine("MENU");
+Console.WriteLine("Presione 1 si es Estudiante");
+Console.WriteLine("Presione 2 si es Docente");
+Console.WriteLine("Presione 3 si es Administrativo");
+Console.WriteLine("Presione 4 si es Externo");
